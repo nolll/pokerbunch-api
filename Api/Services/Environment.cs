0e422d1606b70fa0355bcf14d0f920892e3cc104
Henrik Severinson <henriks@gmail.com>
2018-08-27 23:55:58 +0200

    Changed .lan to .local

--- Api/Services/Environment.cs
+++ Api/Services/Environment.cs
@@ -12,11 +12,11 @@ namespace Api.Services
         public bool IsDevMode => IsDevModeAdmin || IsDevModePlayer;
         public bool IsDevModeAdmin => IsLocal && Contains("api-admin");
         public bool IsDevModePlayer => IsLocal && Contains("api-player");
-        public bool IsDev => EndsWith("pokerbunch.lan");
+        public bool IsDev => EndsWith("pokerbunch.local");
         public bool IsTest => EndsWith("homeip.net");
         public bool IsStage => EndsWith("staging.pokerbunch.com");
         public bool IsAnyTest => IsDev || IsTest || IsStage;
-        private bool IsLocal => EndsWith(".lan");
+        private bool IsLocal => EndsWith(".local");
         private bool EndsWith(string s) => _hostName.EndsWith(s);
         private bool Contains(string s) => _hostName.Contains(s);
     }
