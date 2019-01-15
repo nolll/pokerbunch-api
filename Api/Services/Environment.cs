814667f3e77f2dbac62e9234149f6732a5ddc9dc
Henrik Severinson <henriks@gmail.com>
2019-01-15 22:38:51 +0100

    Handle 192.168.1.* as dev-environment

--- Api/Services/Environment.cs
+++ Api/Services/Environment.cs
@@ -12,11 +12,12 @@ namespace Api.Services
         public bool IsDevMode => IsDevModeAdmin || IsDevModePlayer;
         public bool IsDevModeAdmin => IsLocal && Contains("api-admin");
         public bool IsDevModePlayer => IsLocal && Contains("api-player");
-        public bool IsDev => EndsWith("pokerbunch.local");
-        public bool IsTest => EndsWith("homeip.net");
-        public bool IsStage => EndsWith("staging.pokerbunch.com");
         public bool IsAnyTest => IsDev || IsTest || IsStage;
-        private bool IsLocal => EndsWith(".local");
+        private bool IsDev => EndsWith("pokerbunch.local") || EndsWith("pokerbunch.lan") || StartsWith("192.168.1");
+        private bool IsTest => EndsWith("homeip.net");
+        private bool IsStage => EndsWith("staging.pokerbunch.com");
+        private bool IsLocal => EndsWith(".local") || EndsWith(".lan");
+        private bool StartsWith(string s) => _hostName.StartsWith(s);
         private bool EndsWith(string s) => _hostName.EndsWith(s);
         private bool Contains(string s) => _hostName.Contains(s);
     }
