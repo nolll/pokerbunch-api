5eba76da52e3c8a684b86a708dd803fb315fa5a4
nolll <henriks@gmail.com>
2019-09-13 11:47:02 +0200

    cleanup

--- Api.Core/Bootstrapping/Startup.cs
+++ Api.Core/Bootstrapping/Startup.cs
@@ -3,42 +3,17 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace Api.Bootstrapping
 {
     public class Startup
     {
-        private readonly ILogger _logger;
-        //        private static void ConfigFormatters(HttpConfiguration config)
-        //        {
-        //            var jsonFormatter = new JsonMediaTypeFormatter();
-        //            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-        //            jsonFormatter.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
-        //            jsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json"));
-
-        //            config.Services.Replace(typeof(IContentNegotiator), new JsonContentNegotiator(jsonFormatter));
-
-        //            config.Formatters.Clear();
-        //            config.Formatters.Add(jsonFormatter);
-        //        }
-
         //        private void ConfigureErrorHandler(HttpConfiguration config)
         //        {
         //            config.Services.Replace(typeof(IExceptionHandler), new CustomExceptionHandler());
         //            config.Services.Replace(typeof(IHttpControllerSelector), new HttpNotFoundAwareDefaultHttpControllerSelector(config));
         //            config.Services.Replace(typeof(IHttpActionSelector), new HttpNotFoundAwareControllerActionSelector());
         //        }
-
-        //        private void RemoveUnwantedHeaders(IAppBuilder app)
-        //        {
-        //            app.Use((context, next) =>
-        //            {
-        //                context.Response.Headers.Remove("Server");
-        //                return next.Invoke();
-        //            });
-        //        }
-
 
         private readonly AppSettings _settings;
 
