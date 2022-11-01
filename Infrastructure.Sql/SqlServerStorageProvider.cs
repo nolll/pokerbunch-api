eb6248184206dfb9b7556afc7541d1a68e5b775f
Henrik <henriks@gmail.com>
2022-11-01 23:04:02 +0100

    Cleanup

--- Infrastructure.Sql/SqlServerStorageProvider.cs
+++ Infrastructure.Sql/SqlServerStorageProvider.cs
@@ -23,21 +23,17 @@ public class SqlServerStorageProvider
 
     public IStorageDataReader Query(string sql, IEnumerable<SimpleSqlParameter> parameters = null)
     {
-        using (var connection = GetConnection())
-        {
-            connection.Open();
-            using (var command = new NpgsqlCommand(sql, connection))
-            {
-                if (parameters != null)
-                {
-                    command.Parameters.AddRange(ToSqlCommands(parameters));
-                }
-                var mySqlReader = command.ExecuteReader();
-                var dt = new DataTable();
-                dt.Load(mySqlReader);
-                return new StorageDataReader(dt.CreateDataReader());
-            }
-        }
+        using var connection = GetConnection();
+        connection.Open();
+
+        using var command = new NpgsqlCommand(sql, connection);
+        if (parameters != null)
+            command.Parameters.AddRange(ToSqlCommands(parameters));
+        
+        var mySqlReader = command.ExecuteReader();
+        var dt = new DataTable();
+        dt.Load(mySqlReader);
+        return new StorageDataReader(dt.CreateDataReader());
     }
 
     public IStorageDataReader Query(string sql, ListSqlParameter parameter)
@@ -48,37 +44,29 @@ public class SqlServerStorageProvider
 
     public int Execute(string sql, IEnumerable<SimpleSqlParameter> parameters = null)
     {
-        using (var connection = GetConnection())
-        {
-            connection.Open();
-            using (var command = new NpgsqlCommand(sql, connection))
-            {
-                if (parameters != null)
-                {
-                    command.Parameters.AddRange(ToSqlCommands(parameters));
-                }
-                return command.ExecuteNonQuery();
-            }
-        }
+        using var connection = GetConnection();
+        connection.Open();
+
+        using var command = new NpgsqlCommand(sql, connection);
+        if (parameters != null)
+            command.Parameters.AddRange(ToSqlCommands(parameters));
+
+        return command.ExecuteNonQuery();
     }
 
     public int ExecuteInsert(string sql, IEnumerable<SimpleSqlParameter> parameters = null)
     {
-        using (var connection = GetConnection())
-        {
-            connection.Open();
-            using (var command = new NpgsqlCommand(sql, connection))
-            {
-                if (parameters != null)
-                {
-                    command.Parameters.AddRange(ToSqlCommands(parameters));
-                }
-                return Convert.ToInt32(command.ExecuteScalar());
-            }
-        }
+        using var connection = GetConnection();
+        connection.Open();
+
+        using var command = new NpgsqlCommand(sql, connection);
+        if (parameters != null)
+            command.Parameters.AddRange(ToSqlCommands(parameters));
+
+        return Convert.ToInt32(command.ExecuteScalar());
     }
 
-    private NpgsqlParameter[] ToSqlCommands(IEnumerable<SimpleSqlParameter> parameters)
+    private static NpgsqlParameter[] ToSqlCommands(IEnumerable<SimpleSqlParameter> parameters)
     {
         return parameters.Select(o => o.SqlParameter).ToArray();
     }
