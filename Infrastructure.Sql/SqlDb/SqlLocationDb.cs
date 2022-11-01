4e7acffacc81308288031dc5512250b6a2d9ef69
Henrik <henriks@gmail.com>
2022-11-01 23:32:27 +0100

    Converted SqlLocationDb

--- Infrastructure.Sql/SqlDb/SqlLocationDb.cs
+++ Infrastructure.Sql/SqlDb/SqlLocationDb.cs
@@ -7,8 +7,13 @@ namespace Infrastructure.Sql.SqlDb;
 
 public class SqlLocationDb
 {
-    private const string DataSql = "SELECT l.Id, l.Name, l.BunchId FROM Location l ";
-    private const string SearchIdSql = "SELECT l.Id FROM Location l ";
+    private const string DataSql = @"
+SELECT l.location_id, l.name, l.bunch_id
+FROM pb_location l ";
+
+    private const string SearchIdSql = @"
+SELECT l.location_id
+FROM pb_location l ";
 
     private readonly SqlServerStorageProvider _db;
 
@@ -19,7 +24,7 @@ public class SqlLocationDb
 
     public Location Get(int id)
     {
-        var sql = string.Concat(DataSql, "WHERE l.Id = @id");
+        var sql = string.Concat(DataSql, "WHERE l.location_id = @id");
         var parameters = new List<SimpleSqlParameter>
         {
             new SimpleSqlParameter("@id", id)
@@ -32,7 +37,8 @@ public class SqlLocationDb
     {
         if (!ids.Any())
             return new List<Location>();
-        var sql = string.Concat(DataSql, "WHERE l.Id IN (@ids)");
+
+        var sql = string.Concat(DataSql, "WHERE l.location_id IN (@ids)");
         var parameter = new ListSqlParameter("@ids", ids);
         var reader = _db.Query(sql, parameter);
         return reader.ReadList(CreateLocation);
@@ -40,18 +46,20 @@ public class SqlLocationDb
 
     public IList<int> Find(int bunchId)
     {
-        var sql = string.Concat(SearchIdSql, "WHERE l.BunchId = @bunchId");
+        var sql = string.Concat(SearchIdSql, "WHERE l.bunch_id = @bunchId");
         var parameters = new List<SimpleSqlParameter>
         {
             new SimpleSqlParameter("@bunchId", bunchId)
         };
         var reader = _db.Query(sql, parameters);
-        return reader.ReadIntList("Id");
+        return reader.ReadIntList("bunch_id");
     }
         
     public int Add(Location location)
     {
-        const string sql = "INSERT INTO location (Name, BunchId) VALUES (@name, @bunchId) SELECT SCOPE_IDENTITY() AS [SCOPE_IDENTITY]";
+        const string sql = @"
+INSERT INTO pb_location (name, bunch_id)
+VALUES (@name, @bunchId) RETURNING location_id";
         var parameters = new List<SimpleSqlParameter>
         {
             new SimpleSqlParameter("@name", location.Name),
@@ -63,8 +71,8 @@ public class SqlLocationDb
     private Location CreateLocation(IStorageDataReader reader)
     {
         return new Location(
-            reader.GetIntValue("Id"),
-            reader.GetStringValue("Name"),
-            reader.GetIntValue("BunchId"));
+            reader.GetIntValue("location_id"),
+            reader.GetStringValue("name"),
+            reader.GetIntValue("bunch_id"));
     }
 }
