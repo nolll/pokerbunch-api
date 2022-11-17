bd52dfd4cf4123fc6984f91b094964da612eec24
Henrik <henriks@gmail.com>
2022-11-17 21:55:59 +0100

    Added async usecases

--- Core/UseCases/UseCase.cs
+++ Core/UseCases/UseCase.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Core.Errors;
 
 namespace Core.UseCases;
@@ -39,6 +40,60 @@ public abstract class UseCase<TResult>
         try
         {
             return Work();
+        }
+        catch (Exception e)
+        {
+            return new UseCaseResult<TResult>(new UnknownError(e));
+        }
+    }
+
+    protected UseCaseResult<TResult> Success(TResult result)
+    {
+        return new UseCaseResult<TResult>(result);
+    }
+
+    protected UseCaseResult<TResult> Error(UseCaseError error)
+    {
+        return new UseCaseResult<TResult>(error);
+    }
+}
+
+public abstract class AsyncUseCase<TRequest, TResult>
+{
+    protected abstract Task<UseCaseResult<TResult>> Work(TRequest request);
+
+    public async Task<UseCaseResult<TResult>> Execute(TRequest request)
+    {
+        try
+        {
+            return await Work(request);
+        }
+        catch (Exception e)
+        {
+            return new UseCaseResult<TResult>(new UnknownError(e));
+        }
+    }
+
+    protected UseCaseResult<TResult> Success(TResult result)
+    {
+        return new UseCaseResult<TResult>(result);
+    }
+
+    protected UseCaseResult<TResult> Error(UseCaseError error)
+    {
+        return new UseCaseResult<TResult>(error);
+    }
+}
+
+public abstract class AsyncUseCase<TResult>
+{
+    protected abstract Task<UseCaseResult<TResult>> Work();
+
+    public async Task<UseCaseResult<TResult>> Execute()
+    {
+        try
+        {
+            return await Work();
         }
         catch (Exception e)
         {
