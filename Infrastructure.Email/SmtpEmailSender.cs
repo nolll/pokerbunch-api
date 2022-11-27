9cb82109f6024d472d517dd8869f2f505ed3b44f
Henrik <henriks@gmail.com>
2022-11-27 22:59:14 +0100

    Revert "Revert "Mailgun config""
    This reverts commit 0c5cf385c27bd9baf6f67a79ed74b84449648fb4.

--- Infrastructure.Email/SmtpEmailSender.cs
+++ Infrastructure.Email/SmtpEmailSender.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Net.Mail;
 using Core;
 
@@ -6,19 +7,36 @@ namespace Infrastructure.Email;
 public class SmtpEmailSender : EmailSender
 {
     private readonly string _smtpHost;
+    private readonly int _port;
+    private readonly string _userName;
+    private readonly string _password;
 
-    public SmtpEmailSender(string smtpHost)
+    public SmtpEmailSender(string smtpHost, int port, string userName = null, string password = null)
     {
         _smtpHost = smtpHost;
+        _port = port;
+        _userName = userName;
+        _password = password;
     }
 
     public override void Send(string to, IMessage message)
     {
-        var from = $"{FromName} <{FromEmail}>";
+        const string from = $"{FromName} <{FromEmail}>";
         var email = new MailMessage(from, to, message.Subject, message.Body);
 
         Client.Send(email);
     }
 
-    private SmtpClient Client => new SmtpClient(_smtpHost);
+    private SmtpClient Client
+    {
+        get
+        {
+            var client =  new SmtpClient(_smtpHost);
+            client.Credentials = _userName != null && _password != null
+                ? new NetworkCredential(_userName, _password)
+                : null;
+            client.Port = _port;
+            return client;
+        }
+    }
 }
