534c1fc29bdd7cf3b3c9aac18accd984546fc873
Henrik <henriks@gmail.com>
2023-03-03 23:14:36 +0100

    Moving to new program.cs

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,25 +1,253 @@
-﻿using Api.Bootstrapping;
+﻿using Api.Extensions;
+using Api.Settings;
+using Api.Urls.ApiUrls;
+using Core;
+using Core.Cache;
+using Core.Repositories;
+using Core.Services;
+using Core.UseCases;
+using Infrastructure.Cache;
+using Infrastructure.Email;
+using Infrastructure.Sql;
+using Infrastructure.Sql.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using System.Text;
+using Api.Bootstrapping;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Api.Extensions.Swagger;
+using Microsoft.OpenApi.Models;
+using System.IO;
+using System.Reflection;
+using Api.Middleware;
+using Api.Routes;
 
-namespace Api;
+var builder = WebApplication.CreateBuilder(args);
+var port = Environment.GetEnvironmentVariable("PORT");
+var settings = builder.Configuration.Get<AppSettings>() ?? new AppSettings();
+var isDev = builder.Environment.IsDevelopment();
+var isProd = isDev;
 
-public class Program
+if (!string.IsNullOrEmpty(port))
+    builder.WebHost.UseUrls("http://*:" + port);
+
+var connectionString = GetConnectionString(builder.Configuration);
+
+builder.Services.AddResponseCompression(options =>
 {
-    public static void Main(string[] args)
+    options.EnableForHttps = true;
+    options.Providers.Add<GzipCompressionProvider>();
+});
+
+builder.Services.AddLogging(logging =>
+{
+    if (settings.Logging.Loggers.Debug)
+        logging.AddDebug();
+
+    if (settings.Logging.Loggers.Console)
+        logging.AddConsole();
+
+    logging.SetMinimumLevel(settings.Logging.LogLevel.Default);
+});
+
+builder.Services.AddSingleton(settings);
+builder.Services.AddSingleton<ISettings>(new Settings(settings.InvitationSecret));
+builder.Services.AddSingleton(new UrlProvider(settings.Urls.Api, settings.Urls.Site));
+
+builder.Services.AddSingleton<IAuthorizationHandler, CustomAuthorizationHandler>();
+builder.Services.AddSingleton<ICacheProvider, MemoryCacheProvider>();
+builder.Services.AddSingleton<ICache, Cache>();
+builder.Services.AddSingleton<IUserRepository, UserRepository>();
+builder.Services.AddSingleton<IBunchRepository, BunchRepository>();
+builder.Services.AddSingleton<ICashgameRepository, CashgameRepository>();
+builder.Services.AddSingleton<IEventRepository, EventRepository>();
+builder.Services.AddSingleton<ILocationRepository, LocationRepository>();
+builder.Services.AddSingleton<IPlayerRepository, PlayerRepository>();
+builder.Services.AddSingleton(GetEmailSender(builder.Configuration));
+builder.Services.AddSingleton<IDb>(new PostgresDb(connectionString));
+builder.Services.AddSingleton<IRandomizer, Randomizer>();
+builder.Services.AddSingleton<IInvitationCodeCreator, InvitationCodeCreator>();
+
+// Admin
+builder.Services.AddSingleton<ClearCache>();
+builder.Services.AddSingleton<TestEmail>();
+builder.Services.AddSingleton<RequireAppsettingsAccess>();
+
+// Auth
+builder.Services.AddSingleton<Login>();
+
+// User
+builder.Services.AddSingleton<UserDetails>();
+builder.Services.AddSingleton<UserList>();
+builder.Services.AddSingleton<EditUser>();
+builder.Services.AddSingleton<AddUser>();
+builder.Services.AddSingleton<ChangePassword>();
+builder.Services.AddSingleton<ResetPassword>();
+
+// Bunch
+builder.Services.AddSingleton<GetBunchList>();
+builder.Services.AddSingleton<GetBunchListForUser>();
+builder.Services.AddSingleton<GetBunch>();
+builder.Services.AddSingleton<AddBunch>();
+builder.Services.AddSingleton<EditBunch>();
+
+// Events
+builder.Services.AddSingleton<EventDetails>();
+builder.Services.AddSingleton<EventList>();
+builder.Services.AddSingleton<AddEvent>();
+
+// Locations
+builder.Services.AddSingleton<GetLocationList>();
+builder.Services.AddSingleton<GetLocation>();
+builder.Services.AddSingleton<AddLocation>();
+
+// Cashgame
+builder.Services.AddSingleton<CashgameList>();
+builder.Services.AddSingleton<EventCashgameList>();
+builder.Services.AddSingleton<PlayerCashgameList>();
+builder.Services.AddSingleton<CurrentCashgames>();
+builder.Services.AddSingleton<CashgameDetails>();
+builder.Services.AddSingleton<Buyin>();
+builder.Services.AddSingleton<Report>();
+builder.Services.AddSingleton<Cashout>();
+builder.Services.AddSingleton<AddCashgame>();
+builder.Services.AddSingleton<EditCashgame>();
+builder.Services.AddSingleton<DeleteCashgame>();
+builder.Services.AddSingleton<EditCheckpoint>();
+builder.Services.AddSingleton<DeleteCheckpoint>();
+
+// Player
+builder.Services.AddSingleton<GetPlayer>();
+builder.Services.AddSingleton<GetPlayerList>();
+builder.Services.AddSingleton<AddPlayer>();
+builder.Services.AddSingleton<DeletePlayer>();
+builder.Services.AddSingleton<InvitePlayer>();
+builder.Services.AddSingleton<JoinBunch>();
+
+builder.Services.AddMvc(options =>
+{
+    options.EnableEndpointRouting = false;
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policyBuilder => policyBuilder
+        .SetIsOriginAllowed(_ => true)
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials());
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("UserPolicy", policy => policy.Requirements.Add(new CustomAuthRequirement()));
+});
+
+builder.Services.AddAuthentication(x =>
     {
-        CreateWebHostBuilder(args).Build().Run();
-    }
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(AuthSecretProvider.GetSecret(settings.Auth.Secret))),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            LifetimeValidator = CustomLifetimeValidator
+        };
+    });
 
-    public static IHostBuilder CreateWebHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                var port = Environment.GetEnvironmentVariable("PORT");
+builder.Services.AddSwaggerGen(c =>
+{
+    var assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+    var xmlFile = $"{assemblyName}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Poker Bunch Api", Version = "v1" });
+    c.IncludeXmlComments(xmlPath);
+    c.OperationFilter<AuthorizationHeaderParameterOperationFilter>();
+    c.CustomSchemaIds(SwaggerSchema.GetSwaggerTypeName);
+});
 
-                if (!string.IsNullOrEmpty(port))
-                    webBuilder.UseUrls("http://*:" + port);
-                
-                webBuilder.UseStartup<Startup>();
-            });
+var app = builder.Build();
+
+app.UseCors("CorsPolicy");
+app.UseResponseCompression();
+
+if (isProd)
+{
+    app.UseHsts();
+    app.UseHttpsRedirection();
+}
+
+app.UseMiddleware<SecurityHeadersMiddleware>();
+
+if (settings.Error.DetailedErrors)
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    const string errorUrl = $"/{ApiRoutes.Error}";
+    app.UseStatusCodePagesWithReExecute(errorUrl);
+    app.UseExceptionHandler(errorUrl);
+    app.UseMiddleware<ExceptionLoggingMiddleware>();
+}
+
+app.UseSwagger();
+app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Version 1"); });
+
+app.UseAuthentication();
+app.UseMvc();
+
+static bool CustomLifetimeValidator(DateTime? notBefore, DateTime? expires, SecurityToken tokenToValidate, TokenValidationParameters param)
+{
+    if (expires != null)
+        return expires > DateTime.UtcNow;
+
+    return false;
+}
+
+static string GetConnectionString(IConfiguration configuration)
+{
+    var databaseUrl = configuration.GetValue<string>("DATABASE_URL");
+    if (string.IsNullOrEmpty(databaseUrl))
+        return "";
+
+    var databaseUri = new Uri(databaseUrl);
+    var userInfo = databaseUri.UserInfo.Split(':');
+
+    var connectionStringBuilder = new NpgsqlConnectionStringBuilder
+    {
+        Host = databaseUri.Host,
+        Port = databaseUri.Port,
+        Username = userInfo[0],
+        Password = userInfo[1],
+        Database = databaseUri.LocalPath.TrimStart('/'),
+        TrustServerCertificate = true
+    };
+
+    return connectionStringBuilder.ToString();
+}
+
+static IEmailSender GetEmailSender(IConfiguration configuration)
+{
+    var host = configuration.GetValue<string>("SMTP_SERVER") ?? "localhost";
+    var strPort = configuration.GetValue<string>("SMTP_PORT");
+    var port = strPort != null ? int.Parse(strPort) : 25;
+    var login = configuration.GetValue<string>("SMTP_LOGIN");
+    var password = configuration.GetValue<string>("SMTP_PASSWORD");
+    return new SmtpEmailSender(host, port, login, password);
 }
---
+++ Api/ProgramOld.cs
@@ -0,0 +1,25 @@
+﻿using Api.Bootstrapping;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateWebHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                var port = Environment.GetEnvironmentVariable("PORT");
+
+                if (!string.IsNullOrEmpty(port))
+                    webBuilder.UseUrls("http://*:" + port);
+                
+                webBuilder.UseStartup<Startup>();
+            });
+}
