fe5c84f30d4569d17ef9ca6a1e364f0fe2fc6d17
Henrik Severinson <henriks@gmail.com>
2025-09-14 18:01:17 +0200

    Introducing xunit, autofixture and nsubstitute

--- Tests.Core/UseCases/UserListTests.cs
+++ Tests.Core/UseCases/UserListTests.cs
@@ -1,20 +1,32 @@
-﻿using Core.UseCases;
+﻿using AutoFixture;
+using Core.Entities;
+using Core.Repositories;
+using Core.UseCases;
+using NSubstitute;
 using Tests.Common;
 using Tests.Core.TestClasses;
+using Xunit;
 
 namespace Tests.Core.UseCases;
 
 public class UserListTests : TestBase
 {
-    [Test]
+    [Fact]
     public async Task UserList_ReturnsListOfUserItems()
     {
-        var result = await Sut.Execute(new UserList.Request(new AuthInTest(canListUsers: true)));
+        var fixture = new Fixture();
+        var user1 = fixture.Create<User>();
+        var user2 = fixture.Create<User>();
+        
+        var userRepository = Substitute.For<IUserRepository>();
+        userRepository.List().Returns([user1, user2]);
 
-        result.Data?.Users.Count.Should().Be(4);
-        result.Data?.Users.First().DisplayName.Should().Be(TestData.UserDisplayNameA);
-        result.Data?.Users.First().UserName.Should().Be("user-name-a");
+        var sut = new UserList(userRepository);
+        
+        var result = await sut.Execute(new UserList.Request(new AuthInTest(canListUsers: true)));
+
+        result.Data?.Users.Count.Should().Be(2);
+        result.Data?.Users.First().DisplayName.Should().Be(user1.DisplayName);
+        result.Data?.Users.First().UserName.Should().Be(user1.UserName);
     }
-
-    private UserList Sut => new(Deps.User);
 }
