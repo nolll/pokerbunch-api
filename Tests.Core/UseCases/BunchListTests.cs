52fd3f590ea030980c6d297ba5e5cad5a0aaade8
Henrik Severinson <henriks@gmail.com>
2025-09-18 08:50:29 +0200

    Refactoring tests

--- Tests.Core/UseCases/BunchListTests.cs
+++ Tests.Core/UseCases/BunchListTests.cs
@@ -1,4 +1,6 @@
-﻿using Core.UseCases;
+﻿using Core.Repositories;
+using Core.UseCases;
+using NSubstitute;
 using Tests.Common;
 using Tests.Core.TestClasses;
 
@@ -6,17 +8,25 @@ namespace Tests.Core.UseCases;
 
 public class BunchListTests : TestBase
 {
+    private readonly IBunchRepository _bunchRepository = Substitute.For<IBunchRepository>(); 
+    
     [Test]
     public async Task BunchList_ReturnsListOfBunchItems()
     {
+        var bunch1 = Create.Bunch();
+        var bunch2 = Create.Bunch();
+        _bunchRepository.List().Returns([bunch1, bunch2]);
+        
         var result = await Sut.Execute(new GetBunchList.Request(new AuthInTest(canListBunches: true)));
 
         result.Data!.Bunches.Count.Should().Be(2);
-        result.Data!.Bunches[0].Slug.Should().Be("bunch-a");
-        result.Data!.Bunches[0].Name.Should().Be(TestData.BunchA.DisplayName);
-        result.Data!.Bunches[1].Slug.Should().Be("bunch-b");
-        result.Data!.Bunches[1].Name.Should().Be(TestData.BunchB.DisplayName);
+        result.Data!.Bunches[0].Slug.Should().Be(bunch1.Slug);
+        result.Data!.Bunches[0].Name.Should().Be(bunch1.DisplayName);
+        result.Data!.Bunches[0].Description.Should().Be(bunch1.Description);
+        result.Data!.Bunches[1].Slug.Should().Be(bunch2.Slug);
+        result.Data!.Bunches[1].Name.Should().Be(bunch2.DisplayName);
+        result.Data!.Bunches[1].Description.Should().Be(bunch2.Description);
     }
 
-    private GetBunchList Sut => new(Deps.Bunch);
+    private GetBunchList Sut => new(_bunchRepository);
 }
