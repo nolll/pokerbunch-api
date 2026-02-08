57b06a56884a0bf44b342ffb5dbdc57b4e9e6be6
Henrik Severinson <henriks@gmail.com>
2026-02-08 23:45:29 +0100

    refactor

--- Api/Middleware/SecurityHeadersMiddleware.cs
+++ Api/Middleware/SecurityHeadersMiddleware.cs
@@ -10,25 +10,17 @@ public class SecurityHeadersMiddleware(RequestDelegate next)
     [UsedImplicitly]
     public async Task InvokeAsync(HttpContext httpContext)
     {
-        SetDefaultSecurityHeaders(httpContext);
-        SetCspSecurityHeaders(httpContext);
-        await next(httpContext);
-    }
-
-    private static void SetDefaultSecurityHeaders(HttpContext httpContext)
-    {
         httpContext.AddHeader("X-Content-Type-Options", "nosniff");
         httpContext.AddHeader("X-Frame-Options", "DENY");
         httpContext.AddHeader("X-XSS-Protection", "1; mode=block");
         httpContext.AddHeader("Strict-Transport-Security", "max-age=63072000; includeSubDomains");
+        httpContext.AddHeader("Content-Security-Policy", Csp);
+        await next(httpContext);
     }
 
-    private static void SetCspSecurityHeaders(HttpContext httpContext) => 
-        httpContext.AddHeader("Content-Security-Policy", GetCsp());
+    private static string Csp => string.Join("; ", CspValues);
 
-    private static string GetCsp() => string.Join("; ", GetDefaultCspValues());
-
-    private static string[] GetDefaultCspValues() =>
+    private static string[] CspValues =>
     [
         "default-src 'self'",
         "script-src 'self' 'unsafe-inline'",
